7493b4280db41d160af64f531ec1732938515563
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-03-03 08:14:28 -0600

    changed some configs in pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,15 +1,32 @@
 const pg = require('pg');
 
-const config = {
-  database: process.env.DATABASE_NAME || 'weekend-to-do-app',
-  host: 'localhost',
-  port: 5432,
-};
+if (process.env.DATABASE_URL) {
+  config = {
+    // We use the DATABASE_URL from Heroku to connect to our DB
+    connectionString: process.env.DATABASE_URL,
+    // Heroku also requires this special `ssl` config
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  // If we're not on heroku, configure PG to use our local database
+  config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'weekend-to-do-app', 
+  };
+}
+
+
+// const config = {
+//   database: process.env.DATABASE_NAME || 'weekend-to-do-app',
+//   host: 'localhost',
+//   port: 5432,
+// };
 
 const pool = new pg.Pool(config);
 
 pool.on('connect', () => {
-  // console.log('connected to postgres');
+  console.log('connected to postgres');
 });
 
 pool.on('error', (err) => {
